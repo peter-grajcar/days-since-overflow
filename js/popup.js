2d05b705cef23af978090444c9ae5351464d2333
Peter Grajcar <peter.grajcar131@gmail.com>
2021-04-02 18:49:04 +0200

    Fix unsafe innerHTML assignment
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,7 @@ function setDays(historyItems) {
     let lastVisit = new Date(historyItems[0].lastVisitTime);
     let today = new Date();
     let daysSinceLastVisit = Math.floor((today - lastVisit) / (1000 * 60 * 60 * 24));
-    days.innerHTML = daysSinceLastVisit;
+    days.textContent = daysSinceLastVisit;
 }
 
 browser.history.search({
